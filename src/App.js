ec7c046dbfa68e6a56ae5a2d945954003479db2e
ethanlee <seungwoonlee90@gmail.com>
2022-10-29 21:23:24 +0900

    Create components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import Modal from "./components/Modal";
+import Backdrop from "./components/Backdrop";
 import Todo from "./components/Todo";
 
 function App() {
   return (
     <div>
       <h1>My Tods</h1>
-      <Todo />
+      <Todo text="Learn React" />
+      <Todo text="Learn NextJS" />
+      <Modal />
+      <Backdrop />
     </div>
   );
 }
