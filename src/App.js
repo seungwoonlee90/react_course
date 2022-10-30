9933948808d56087eba54b15ddd51619a1d892c5
ethanlee <seungwoonlee90@gmail.com>
2022-10-30 16:39:13 +0900

    wrapper and props

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import { Route, Routes } from "react-router-dom";
-import MainNavigation from "./components/layout/MainNavigation";
+import Layout from "./components/layout/Layout";
 import AllMeetupsPage from "./pages/AllMeetups";
 import Favorites from "./pages/Favorites";
 import NewMeetups from "./pages/NewMeetup";
 
 function App() {
   return (
-    <div>
-      <MainNavigation />
+    <Layout>
       <Routes>
         <Route path="/" element={<AllMeetupsPage />} />
         <Route path="/new-meetup" element={<NewMeetups />} />
         <Route path="/favorites" element={<Favorites />} />
       </Routes>
-    </div>
+    </Layout>
   );
 }
 
