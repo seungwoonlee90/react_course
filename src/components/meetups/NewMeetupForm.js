9d629509b05b6dde10afeaad4c18fe01627ead22
ethanlee <seungwoonlee90@gmail.com>
2022-10-30 19:42:57 +0900

    NewMeetupForm & useRef

---
+++ src/components/meetups/NewMeetupForm.js
@@ -0,0 +1,58 @@
+import { useRef } from "react";
+import classes from "./NewMeetupForm.module.css";
+import Card from "../ui/Card";
+
+export default function NewMeetupForm() {
+  const titleInputRef = useRef();
+  const imageInputRef = useRef();
+  const addressInputRef = useRef();
+  const descriptionInputRef = useRef();
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const enteredTitle = titleInputRef.current.value;
+    const enteredImage = imageInputRef.current.value;
+    const enteredAddress = addressInputRef.current.value;
+    const enteredDescription = descriptionInputRef.current.value;
+
+    const meetupData = {
+      title: enteredTitle,
+      image: enteredImage,
+      address: enteredAddress,
+      description: enteredDescription,
+    };
+    console.log(meetupData);
+  };
+
+  return (
+    <Card>
+      <form className={classes.form} onSubmit={submitHandler}>
+        <div className={classes.control}>
+          <label htmlFor="title">Meetup Title</label>
+          <input type="text" id="title" required ref={titleInputRef} />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="image">Meetup Image</label>
+          <input type="url" id="image" required ref={imageInputRef} />
+        </div>
+
+        <div className={classes.control}>
+          <label htmlFor="address">Meetup Address</label>
+          <input type="text" id="address" required ref={addressInputRef} />
+        </div>
+
+        <div className={classes.control}>
+          <label htmlFor="description">Meetup Description</label>
+          <textarea
+            id="description"
+            row="5"
+            required
+            ref={descriptionInputRef}
+          ></textarea>
+        </div>
+        <div className={classes.actions}>
+          <button>Add Meetup</button>
+        </div>
+      </form>
+    </Card>
+  );
+}
--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,3 +1,10 @@
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
 export default function NewMeetups() {
-  return <div>New Meetups Page</div>;
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm />
+    </section>
+  );
 }
